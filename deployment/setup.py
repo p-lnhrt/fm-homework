18ec5a8557e83a04e435f88ffe53ea9cd737608e
p-lnhrt <pierre.lienhart@gmail.com>
2020-05-03 17:36:47 +0200

    Add setup.py

---
+++ deployment/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+import predictionws
+
+setuptools.setup(
+    name='ddog',
+    version=predictionws.__version__,
+    author='Pierre LIENHART',
+    author_email='pierre.lienhart@gmail.com',
+    description='FairMoney ML Engineer Homework - Web Service'
+)
